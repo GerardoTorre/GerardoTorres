a5d9bfff295fc2c30016d776df0caec36f4b6a18
Gerardo Torres <martimdesantos@gmail.com>
2024-03-13 03:39:28 +0000

    3

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-function rodarOTexto() {
-    let rodape = document.querySelector("Header");
-    rodape.style.transform = "rotate(100deg)"; // Aplicando uma rotação de 100 graus
-}
-
-setInterval(rodarOTexto, 50);
+setInterval(alert("123"), 3)
